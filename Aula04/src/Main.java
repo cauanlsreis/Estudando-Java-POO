6f85e1f2dea8b0f79fafc6a76c18c68513cfb2a9
cauanlsreis <cauanlucas.reis@gmail.com>
2024-01-05 23:33:01 -0300

    Adicionando aula 4

---
+++ Aula04/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarela", 0.4f);
+        c1.status();
+
+        Caneta c2 = new Caneta("Faber", "Laranja", 1.5f);
+        c2.status();
+
+
+    }
+}
