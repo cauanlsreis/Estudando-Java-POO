490f593ebd7865bff0a7e38211fea047dc77eda9
cauanlsreis <cauanlucas.reis@gmail.com>
2024-03-01 14:09:16 -0300

    Aula 12 Polimorfismo Parte 1

---
+++ Aula12/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //Animal n = new Animal();
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+
+        Canguru c = new Canguru();
+        Cachorro k = new Cachorro();
+        Cobra j = new Cobra();
+        Tartaruga t = new Tartaruga();
+        Goldfish g = new Goldfish();
+        Arara e = new Arara();
+
+        k.locomover();
+        k.emitirSom();
+        c.emitirSom();
+    }
+}
