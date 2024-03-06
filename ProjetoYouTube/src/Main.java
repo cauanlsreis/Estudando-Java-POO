67285f3fc83b245eef0ac62fcdd81f7dd0fe40cf
cauanlsreis <cauanlucas.reis@gmail.com>
2024-03-06 07:56:05 -0300

    Aula 15

--- ProjetoYouTube/src/Main.java
+++ ProjetoYouTube/src/Main.java
@@ -10,8 +10,14 @@ public class Main {
         g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
         g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
 
-        System.out.println(v[0].toString());
-        System.out.println(g[0].toString());
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]); //Jubileu assiste HTML5
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(g[0], v[1]); //Jubileu assiste PHP
+        vis[1].avaliar(87.0f);
+        System.out.println(vis[1].toString());
 
     }
 }
